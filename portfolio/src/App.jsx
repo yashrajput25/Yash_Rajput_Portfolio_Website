7eb25c1ea686f390ac90d0c3bc1132c7b425aba3
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-08-10 18:39:17 +0530

    Add Tailwind CSS v4

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import './App.css'
-
-const App = ()=>{
-  return <main>
-  <div>
-    <h1>Hello</h1>
-  </div>
-  </main>
-
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-amber-300">
+      <h1 className="text-4xl font-bold text-amber-950">
+        Hello this is Yash
+      </h1>
+    </div>
+  )
 }
 
-export default App
+export default App;
+
