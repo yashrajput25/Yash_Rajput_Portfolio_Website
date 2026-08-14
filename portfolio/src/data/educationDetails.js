a7e0bdc224387fe7be67b1add9e6902dc73e8670
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-08-14 23:11:00 +0530

    Create educationDetails.js

---
+++ portfolio/src/data/educationDetails.js
@@ -0,0 +1,35 @@
+import Amity from '../assets/amity.jpeg';
+import IIITD from '../assets/iiitd.jpeg';
+import School from '../assets/school.jpg';
+
+
+const educationDetails = [
+
+    {
+        year: "2024-2026",
+        degree: "M.Tech in Computer Science",
+        college : "Indraprastha Institute of Information Technology",
+        location: "Delhi",
+        link: IIITD
+
+    },
+        {
+        year: "2020-2024",
+        degree: "B.Tech in Computer Science",
+        college : "Amity School of Engineering & Technology",
+        location: "Noida",
+        link: Amity
+
+    },
+        {
+        year: "2020",
+        degree: "Class 12",
+        college : "Manav Sthali School",
+        location: "Delhi",
+        link: School
+
+    }
+
+];
+
+export default educationDetails;
