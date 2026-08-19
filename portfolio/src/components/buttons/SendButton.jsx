14f7eb88fe4e6b30013e3b7d5698140c40fcc032
Yash Rajput <74948146+yashrajput25@users.noreply.github.com>
2026-08-19 15:52:37 +0530

    Update SendButton.jsx

--- portfolio/src/components/buttons/SendButton.jsx
+++ portfolio/src/components/buttons/SendButton.jsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const SendButton = () => {
   return (
     <StyledWrapper>
-      <button>
+      <button type= "submit">
         <div className="svg-wrapper-1">
           <div className="svg-wrapper">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24}>
